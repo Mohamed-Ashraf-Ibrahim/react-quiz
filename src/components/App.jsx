7dd4659ac5205565f2fd32fae57b999416760295
Mohamed Ashraf <mohamed.ash.1523@gmail.com>
2024-08-18 20:34:39 +0300

    Add quiz functionality and responsive styles
    - Added json-server dependency and preview script to package.json for serving quiz questions.
    - Implemented reducer and state management in App component to handle quiz state.
    - Fetch quiz questions on component mount and handle errors.
    - Created Options component for displaying question options as buttons.
    - Developed Start component to display the current question and options.
    - Implemented StartScreen component to provide a welcome message and start button for the quiz.
    - Enhanced responsive styles for various screen sizes to improve layout and typography."

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,79 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+
+  // 'Loading', 'error', 'ready', 'active', 'finished'
+  status: "Loading",
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch("http://localhost:9000/questions");
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching questions!");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="app">
       <Header />
 
       <Main className="main">
-        <p>1/15</p>
-        <p>Questions??</p>
+        {status === "Loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
